2a34b654aecfa9e21e834c54823f4e3ef604f65f
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-03-28 15:25:00 +0900

    250328

---
+++ Ex05_jspMybatis/src/main/java/com/study/springboot/domain/Board.java
@@ -0,0 +1,13 @@
+package com.study.springboot.domain;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	private int boardno;
+	private String title;
+	private String writer;
+	private String content;
+	
+	
+}
