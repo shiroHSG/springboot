dda10715968b7bdb286fd063024f83d631fba1e9
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-03-28 10:43:56 +0900

    250328 update

---
+++ Ex07_jpa/src/test/java/com/example/demo/Ex07JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex07JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
